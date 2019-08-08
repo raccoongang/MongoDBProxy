95bc3be34074335586d75e783664693e6913b866
Feanil Patel <feanil@edx.org>
2019-08-08 14:53:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,11 @@ setup(
     long_description=LONG_DESCRIPTION,
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
     setup_requires=['pyandoc'],
     install_requires=['pymongo'],
