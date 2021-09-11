696429f4112405cc3e411502734248927797c825
Andrey Kryachko <andrey.kryachko@raccoongang.com>
2021-09-11 21:30:00 +0300

    chore: [MON-1533] removed pandoc requirements (too heavy and requires additional Ubuntu packages)

--- setup.py
+++ setup.py
@@ -22,18 +22,7 @@ README_MARKDOWN = None
 with open('README.md') as markdown_source:
     README_MARKDOWN = markdown_source.read()
 
-try:
-    import pandoc
-    pandoc.core.PANDOC_PATH = 'pandoc'
-    # Converts the README.md file to ReST, since PyPI uses ReST for formatting,
-    # This allows to have one canonical README file, being the README.md
-    doc = pandoc.Document()
-    doc.markdown = README_MARKDOWN
-    LONG_DESCRIPTION = doc.rst
-except ImportError:
-    # If pandoc isn't installed, e.g. when downloading from pip,
-    # just use the regular README.
-    LONG_DESCRIPTION = README_MARKDOWN
+LONG_DESCRIPTION = README_MARKDOWN
 
 setup(
     name='MongoDBProxy',
@@ -45,7 +34,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
     ],
-    setup_requires=['pyandoc'],
     install_requires=['pymongo'],
     url="https://github.com/arngarden/MongoDBProxy"
 )
